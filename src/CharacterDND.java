94728dcaf67b8ea5d4765b14321222da34bd1a2f
marinalui <72895343+marinalui@users.noreply.github.com>
2024-02-24 15:06:56 -0500

    create character variables

--- src/CharacterDND.java
+++ src/CharacterDND.java
@@ -1,6 +1,18 @@
-public class CharacterDND {
-    public CharacterDND(){
+import java.util.ArrayList;
 
+public class CharacterDND {
+    private int age;
+    private int level;
+    private Class classType;
+    private Race raceType;
+    private int maxHitPoints;
+    private int currentHitPoints;
+    private ArrayList<String> inventory;
+    public CharacterDND(Class classType, Race raceType){
+        this.classType=classType;
+        this.raceType = raceType;
+        currentHitPoints = maxHitPoints;
     }
+
 
 }
