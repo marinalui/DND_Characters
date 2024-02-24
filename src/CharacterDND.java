6c94d1569ffbb65e66a5344aa245d6dc2b964943
maa1446 <maa1446@rit.edu>
2024-02-24 16:39:57 -0500

    Added height and weight ranges

--- src/CharacterDND.java
+++ src/CharacterDND.java
@@ -3,6 +3,9 @@ import java.util.Random;
 
 public class CharacterDND {
     private int age;
+    private int height;
+    private int weight;
+
     private int level;
     private Class classType;
     private Race raceType;
@@ -19,30 +22,50 @@ public class CharacterDND {
         switch(raceType){
             case DRAGONBORN:
                 age = rand.nextInt(81);
+                height = rand.nextInt(68, 82);
+                weight = rand.nextInt(179, 367);
                 break;
             case DWARF:
                 age = rand.nextInt(351);
+                // Hill Dwarf stats
+                height = rand.nextInt(46, 52);
+                weight = rand.nextInt(119, 211);
                 break;
             case ELF:
                 age = rand.nextInt(751);
+                // High elf stats
+                height = rand.nextInt(56, 74);
+                weight = rand.nextInt(92, 170);
                 break;
             case GNOME:
                 age = rand.nextInt(501);
+                height = rand.nextInt(37, 43);
+                weight = rand.nextInt(37, 43);
                 break;
             case HALFELF:
                 age = rand.nextInt(181);
+                height = rand.nextInt(59, 73);
+                weight = rand.nextInt(114, 238);
                 break;
             case HALFLING:
                 age = rand.nextInt(251);
+                height = rand.nextInt(33, 39);
+                weight = rand.nextInt(37, 43);
                 break;
             case HALFORC:
                 age = rand.nextInt(76);
+                height = rand.nextInt(60, 78);
+                weight = rand.nextInt(144, 380);
                 break;
             case HUMAN:
                 age = rand.nextInt(100);
+                height = rand.nextInt(58, 76);
+                weight = rand.nextInt(114, 270);
                 break;
             case TIEFLING:
                 age = rand.nextInt(106);
+                height = rand.nextInt(59, 73);
+                weight = rand.nextInt(114, 238);
                 break;
         }
     }
